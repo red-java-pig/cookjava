24323fe089f4fb322908d7dc6076c62b1a045004
redpig <lihuang_man@hotmail.com>
2019-03-17 15:27:39 +0800

    add

--- src/stream/HexToKanji.java
+++ src/stream/HexToKanji.java
@@ -2,6 +2,9 @@ package stream;
 
 import java.io.UnsupportedEncodingException;
 
+/**
+ * 将3个十六进制转换位汉字
+ */
 public class HexToKanji {
     public static void main(String[] args) throws UnsupportedEncodingException {
         byte[] bytes = {(byte) 0xE5, (byte) 0xB1, (byte) 0x8C};
